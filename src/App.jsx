15caad7bc6a44b5fe83959d72f0a92f30745d1ae
Sovar Robert Ionut <sovar_robert_ionut@yahoo.com>
2025-04-16 20:52:22 +0300

    Add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import React from "react";
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>Main page</h1>
-      </div>
-    </>
+    <div>
+      <h1>Main page</h1>
+    </div>
   );
 }
 
