c930a4ff9b6a61bb7e72f960a8de1c83aeb07b03
Sovar Robert Ionut <sovar_robert_ionut@yahoo.com>
2025-04-18 11:37:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import NoPage from "./pages/NoPage";
 import SkipPage from "./components/SkipPage";
-import { useNavigate } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Dashboard />
-
-              <div className="flex justify-center items-center mt-4">
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white s py-2 px-4 rounded cursor-pointer"
-                  onClick={() => navigate("/skip")}
-                >
-                  Go to skip page
-                </button>
-              </div>
-            </>
-          }
-        >
+        <Route path="/" element={<Dashboard />}>
           <Route
             path="/skip"
             element={
