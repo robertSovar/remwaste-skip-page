a6ed5b8dbd2001e924609d46cc4e65c87a40b7a8
Sovar Robert Ionut <sovar_robert_ionut@yahoo.com>
2025-04-18 11:05:03 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/remwaste-skip-page/",
+  base: "/remwaste-skip-page/skip",
 });
